9b17bf65453e03d5ce9eebfa8327a8c3fed3c4d1
Wordpress Dev. JB <41737457+wpdevjb@users.noreply.github.com>
2018-07-26 13:24:55 +0200

    Create Custom-Fields-Search.php

---
+++ Custom-Fields-Search.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Joindre les postes et les tables postmeta
+ */
+
+function metabox_search_join( $join ) {
+    global $wpdb;
+
+    if ( is_search() ) {
+        $join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id';
+    }
+
+    return $join;
+}
+add_filter('posts_join', 'metabox_search_join' );
+
+/**
+ * Modifier la requête de recherche avec posts_where
+ */
+
+function metabox_search_where( $where ) {
+    global $pagenow, $wpdb;
+
+    if ( is_search() ) {
+        $where = preg_replace(
+            "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+    }
+
+    return $where;
+}
+add_filter( 'posts_where', 'metabox_search_where' );
+
+function metabox_search_distinct( $where ) {
+    global $wpdb;
+
+    if ( is_search() ) {
+        return "DISTINCT";
+    }
+
+    return $where;
+}
+add_filter( 'posts_distinct', 'metabox_search_distinct' );
